a4fa942ce6ed2fbf4595aa8456470d90771eea4d
Goli Swetha <goliswetha98055@gmail.com>
2023-09-01 00:12:06 +0530

    Initial commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+//import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
